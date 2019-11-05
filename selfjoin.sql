be94b304c9a78ebffde3c252efde948bf77c871c
theVietgamer <51430319+theVietgamer@users.noreply.github.com>
2019-11-05 14:04:38 -0800

    Update selfjoin.sql

--- selfjoin.sql
+++ selfjoin.sql
@@ -1,7 +1,11 @@
 --Jason Nguyen
 --Murach's textbook
-select Distinct Vendors1.VendorName, Vendors1.VendorCity, Vendors1.VendorState --self join that returns vendors from cities in common with other vendors
-From Vendors as vendors1 Join vendors as vendors2       -- command requirements in order to successfully code the innerjoin.
+
+--self join that returns vendors from cities in common with other vendors
+select Distinct Vendors1.VendorName, Vendors1.VendorCity, Vendors1.VendorState 
+
+ -- command requirements in order to successfully code the innerjoin.
+From Vendors as vendors1 Join vendors as vendors2      
 	on (Vendors1.VendorCity = Vendors2.VendorCity) and
 	(Vendors1.VendorState = vendors2.VendorState) and
 	(vendors1.VendorID <> Vendors2.VendorID)
