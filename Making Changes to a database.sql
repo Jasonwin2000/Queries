1b7825cab453faf48cefd558ed52a572aa60586d
theVietgamer <51430319+theVietgamer@users.noreply.github.com>
2019-11-05 13:26:01 -0800

    Add files via upload

--- Making Changes to a database.sql
+++ Making Changes to a database.sql
@@ -1,15 +1,15 @@
 select * from Customers -- here only to look if the code below has done it's thing
-insert into Customers(CustomerFirst,CustomerLast, CustAddr, CustCity, CustState,CustZip,CustPhone,Age)--New adjustment to table
+insert into Customers(CustomerFirst,CustomerLast, CustAddr, CustCity, CustState,CustZip,CustPhone,Age,MiddleName)--New adjustment to table
 
-values('Jason','Nguyen','6437 Jeffrey Ct SE','Lacey','WA',98513, 3603590794,19)
+values('Jason','Nguyen','6437 Jeffrey Ct SE','Lacey','WA',98513, 3603590794,19,'j')
 
-insert into Customers(CustomerFirst,CustomerLast, CustAddr, CustCity, CustState,CustZip,CustPhone,Age)--New adjustment to table
+insert into Customers(CustomerFirst,CustomerLast, CustAddr, CustCity, CustState,CustZip,CustPhone,Age,MiddleName)--New adjustment to table
 
-values('Hien','Nguyen','6437 Jeffrey Ct SE','Lacey','WA',98513, 3608883700,53)
+values('Hien','Nguyen','6437 Jeffrey Ct SE','Lacey','WA',98513, 3608883700,53,'s')
 
-insert into Customers(CustomerFirst,CustomerLast, CustAddr, CustCity, CustState,CustZip,CustPhone,Age)--New adjustment to table
+insert into Customers(CustomerFirst,CustomerLast, CustAddr, CustCity, CustState,CustZip,CustPhone,Age,MiddleName)--New adjustment to table
 
-values('Trang','Nguyen','6437 Jeffrey Ct SE','Lacey','WA',98513, 3608883700,70)
+values('Trang','Nguyen','6437 Jeffrey Ct SE','Lacey','WA',98513, 3608883700,70,'h')
 
 Alter table Customers --made a new column called Middle Name
 add MiddleName character
@@ -19,5 +19,3 @@ drop Column MiddleName
 
 alter table Customers
 add Age int
-
-
