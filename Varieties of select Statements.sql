dd9a8e162e7ddce6efe50c05ce9de524ce0436c0
theVietgamer <51430319+theVietgamer@users.noreply.github.com>
2019-11-05 14:02:17 -0800

    Update Varieties of select Statements.sql

--- Varieties of select Statements.sql
+++ Varieties of select Statements.sql
@@ -1,10 +1,16 @@
 --Jason Nguyen
 --Murach's textbook
-select * from Customers -- displays everything within the database "Customers"
 
-select Distinct CustState from Customers -- only prints out CustStates as the column
+-- displays everything within the database "Customers"
+select * from Customers 
 
-Select TOP 5 CustID from Customers -- selects the first 5 in the database and displays them.
+-- only prints out CustStates as the column
+select Distinct CustState from Customers 
 
-select top 5 with Ties CustID from Customers 0 -- in order for ties to work in the select statement, 
-order by CustomerLast							-- a order by statement is required
+-- selects the first 5 in the database and displays them.
+Select TOP 5 CustID from Customers 
+
+-- in order for ties to work in the select statement, 
+select top 5 with Ties CustID from Customers 0 
+-- a order by statement is required
+order by CustomerLast							
