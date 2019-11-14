e082324110616ea8c404d7244ee53c09a4057971
Jason Nguyen <51430319+Jasonwin2000@users.noreply.github.com>
2019-11-14 13:43:27 -0800

    Add files via upload

--- restore_backup.sql
+++ restore_backup.sql
@@ -1,42 +1,30 @@
---Jason Nguyen
---Murach's textbook
-use master
---this whole procedure is to copy one database into a new one
+--First create a database to put in old database information.
+create database rest0re
+
+-- Find a directory in file explore to back up the old database.
+backup database Examples to disk = 'C:\Temp\Examples.bak'
 
 
----step1:
---code below is a process that copys and backup a certain database in the C drive.
---backup database Examples to  disk = 'C:\Temp\Examples.bak'
-
---- Step2:
---Creating a new database to put information in to restore the program above
-
---create database Jason_practice
---sp stands for store_procedure ;shows what kind of database within the System.
---sp_helpdb
-
----Step3:
---Look for LogicalName for the datafile and log file for the database "example"
-restore filelistonly from  disk = 'C:\Temp\Examples.bak'
+sp_helpdb
 Examples
-Examples_log
 
----Step4:
--- type the database you want to restore in; to get the directory of the new database by typing "sp_helpfile"
---After finding out the directory of the new database, copy it
-use Jason_practice
+
+/*1.Change pathing to the new database.
+2.Go into the new database and get the directory of the original and log form 
+of the new database by typing " sp_helpfile "*/
+
+use rest0re
+C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\rest0re.mdf
+C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\rest0re_log.ldf
 sp_helpfile
 
-C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\Jason_practice.mdf
-C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\Jason_practice_log.ldf
+/* go back to the pathing master in order to copy the data from old to new.
+use the following coding below to succesffuly restore a old database into
+a new database.*/
+use master
+go 
+restore database rest0re from disk = 'C:\Temp\Examples.bak'
+with
+move 'Examples' to 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\rest0re.mdf',
+move 'Examples_log' to 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\rest0re_log.ldf', replace
 
----Step5:
---from here, copy the following statement: '*LogicalNameex1' to 'directoryofnewDB_ex1',
---move 'LogicalNameex2' to 'directoryofnewDB_ex2', replace
---make sure to use master and not any other db or else this would not work.
---use master
---go
---restore database Jason_practice from  disk = 'C:\Temp\Examples.bak'
---with
---move 'Examples' to 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\Jason_practice.mdf',
---move 'Examples_log' to 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\Jason_practice_log.ldf', replace 
